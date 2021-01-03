558565889614460d7dab9acb1a47719c34550719
Sherzod Usmonov <sherzod.usmon.91@gmail.com>
2021-01-04 04:08:40 +0500

    added dateformat

---
+++ src/filters/date.filter.js
@@ -0,0 +1,14 @@
+export default function(value, format = "date") {
+    const options = {}
+    if (format.includes('date')) {
+        options.year = 'numeric'
+        options.month = 'long'
+        options.day = '2-digit'
+    }
+    if (format.includes('time')) {
+        options.hour = '2-digit'
+        options.minute = '2-digit'
+        options.second = '2-digit'
+    }
+    return new Intl.DateTimeFormat("ru-RU", options).format(new Date(value))
+}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import dateFormat from '@/filters/date.filter'
 import router from './router'
 import store from './store'
 import 'materialize-css/dist/js/materialize'
 
 Vue.config.productionTip = false
+
+Vue.filter('date', dateFormat)
 
 new Vue({
   router,
